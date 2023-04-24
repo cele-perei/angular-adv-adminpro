7ec79391ce64299988bfcf5b33389675166febd2
cele-perei <celesteavei@gmail.com>
2023-04-24 12:25:34 -0400

    Fin sección 8

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -17,5 +17,7 @@ export class PagesComponent {
   }
   ngOnInit(): void {
     customInitFunctions();
+    customInitFunctions();
+
   }
 }
--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { ImplicitReceiver } from '@angular/compiler';
+import { Component, OnDestroy } from '@angular/core';
+import { Router, ActivationEnd } from '@angular/router';
+import {Subscription, filter, map} from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -6,6 +9,32 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class BreadcrumbsComponent {
+export class BreadcrumbsComponent implements OnDestroy {
+
+  public titulo: string = '';
+  public tituloSubs$: Subscription;
+
+  constructor(private router:Router) {
+
+    this.tituloSubs$ = this.getArgumentosRuta()
+                    .subscribe( ({titulo}) =>{
+                      this.titulo = titulo;
+                      document.title = `AdminPro - ${titulo}`;
+                    });
+  }
+  ngOnDestroy(): void { 
+    this.tituloSubs$.unsubscribe();
+   }
+
+  getArgumentosRuta () {
+    return this.router.events
+    .pipe(
+      filter((event): event is ActivationEnd => event instanceof ActivationEnd),
+      filter((event:ActivationEnd) => event.snapshot.firstChild === null ),
+      map((event:ActivationEnd) => event.snapshot.data)
+    );
+  }
 
 }
+
+
