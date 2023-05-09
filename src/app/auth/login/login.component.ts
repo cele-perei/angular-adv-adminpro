94ffc76beb0c850935914d43088e305abaa2d2e8
cele-perei <celesteavei@gmail.com>
2023-05-08 21:18:38 -0400

    Login y Auth

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,90 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, OnInit, ViewChild, ElementRef, NgZone } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2'
+
+import { LoginForm } from 'src/app/interfaces/login-form.interface';
+import { UsuarioService } from 'src/app/services/usuario.service';
+
+declare const google:any;
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: [ './login.component.css' ]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, AfterViewInit{
 
-  constructor( private router: Router ){}
+  // @ViewChild('googleBtn') declare googleBtn: ElementRef ;
+  @ViewChild('googleBtn') googleBtn!: ElementRef;
+
+  
+  public formSubmitted = false;
+
+  public loginForm = this.fb.group({
+    email: [localStorage.getItem('email') || '', [ Validators.required, Validators.email ] ],
+    password: ['', Validators.required ],
+    remember: [false]
+  });
+
+
+  constructor( private router: Router,
+              private fb: FormBuilder,
+              private usuarioService: UsuarioService,
+              private ngZone: NgZone){}
+  ngAfterViewInit(): void {
+  }
+  ngOnInit(): void {
+    this.googleInit();
+  }
+
+  
+  googleInit () {
+    google.accounts.id.initialize({
+      client_id: '273296174498-80cikj2oejdvtfv24mmdhl5d7bg6nadk.apps.googleusercontent.com',
+      callback: (response:any) => this.handleCredentialResponse(response)
+    });
+    google.accounts.id.renderButton(
+       document.getElementById("buttonDiv"),
+      //this.googleBtn.nativeElement,
+      { theme: "outline", size: "large" }  // customization attributes
+    );
+  }
+
+  handleCredentialResponse ( response: any ){
+    console.log("Encoded JWT ID token: " + response.credential);
+    this.usuarioService.loginGoogle(response.credential).subscribe( resp => {
+          //Navegar al Dashboard
+          this.ngZone.run( () => {
+            this.router.navigateByUrl('/');
+
+          });    } )
+
+  }
+
   login(){
-    this.router.navigateByUrl('/');
+    
+    this.usuarioService.login( this.loginForm.value as LoginForm )
+        .subscribe( resp => {
+          if ( this.loginForm.get('remember')?.value ){
+              localStorage.setItem('email', this.loginForm.get('email')?.value || '' );
+          } else {
+            localStorage.removeItem('email');
+          }
+
+          //Navegar al Dashboard
+          this.ngZone.run( () => {
+            this.router.navigateByUrl('/');
+
+          });
+
+        }, (err) => {
+          //si sucede un error
+          Swal.fire('Error', err.error.msg, 'error');
+        } );
+                        
+                         
+    //console.log( this.loginForm.value );
+    
   }
 }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import Swal from 'sweetalert2'
+
+import { RegisterForm } from 'src/app/interfaces/register-form.interface';
+import { UsuarioService } from '../../services/usuario.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +13,82 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  public formSubmitted = false;
+
+  public registerForm = this.fb.group({
+    nombre: ['Celeste', Validators.required ],
+    email: ['test60@gmail.com', [ Validators.required, Validators.email ] ],
+    password: ['123456', Validators.required ],
+    password2: ['123456', Validators.required ],
+    terminos: [true, Validators.required ],
+  }, {
+    validators: this.passwordsIguales('password','password2')
+  });
+
+  constructor ( private fb: FormBuilder, 
+                private usuarioService: UsuarioService,
+                private router : Router){}
+
+  crearUsuario() {
+    this.formSubmitted = true;
+     console.log(this.registerForm.value);
+    //console.log(this.registerForm);
+
+    if(this.registerForm.invalid){
+      return;
+    }
+
+    //realizar el posteo
+    //this.usuarioService.crearUsuario( this.registerForm.value )
+    this.usuarioService.crearUsuario( (this.registerForm.value as RegisterForm) )
+      .subscribe( resp => {
+        
+          //Navegar al Dashboard
+          this.router.navigateByUrl('/');
+
+      }, (err) => {
+        //si sucede un error
+        Swal.fire('Error', err.error.msg, 'error');
+      } );
+  }
+
+  campoNoValido( campo: string ): boolean {
+    if( !this.registerForm.get(campo)?.valid && this.formSubmitted){
+      return true;
+
+    } else{
+      return false;
+    }
+  }
+
+  aceptaTerminos(){
+    return !this.registerForm.get('terminos')?.value && this.formSubmitted
+  }
+
+  contrasenasNoValidas(){
+    const pass1 = this.registerForm.get('password')?.value;
+    const pass2 = this.registerForm.get('password2')?.value;
+
+    if( (pass1 !== pass2 ) && this.formSubmitted ){
+      return true; 
+    } else {
+      return false;
+    }
+  }
+
+  passwordsIguales(pass1: string, pass2: string) {
+    
+    return (FormGroup: AbstractControl): ValidationErrors | null => {
+      const pass1Control: FormControl = FormGroup.get(pass1) as FormControl;
+      const pass2Control: FormControl = FormGroup.get(pass2) as FormControl;
+ 
+      if (pass1Control.value !== pass2Control.value) {
+        pass2Control.setErrors({ noEsIgual: true });
+        return { noEsIgual: true };
+      } else {
+        pass2Control?.setErrors(null);
+        return null;
+      }
+    };
+  }
 }
